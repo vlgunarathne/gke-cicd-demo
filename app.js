29cf0856a2ac0c2d783ed131c6e7e380fd281f81
Viduranga <vlgunarathne@gmail.com>
2024-11-27 08:08:18 +1100

    Implement an im-memory issue management app

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Logger middleware
+app.use(logger('combined'));
+
+let issues = [];
+
+// Create an issue
+app.post('/issues', (req, res) => {
+  const issue = req.body;
+  issue.id = issues.length + 1;
+  issues.push(issue);
+  res.status(201).send(issue);
+});
+
+// View all issues
+app.get('/issues', (req, res) => {
+  res.send(issues);
+});
+
+// View a single issue
+app.get('/issues/:id', (req, res) => {
+  const issue = issues.find(i => i.id === parseInt(req.params.id));
+  if (!issue) return res.status(404).send('Issue not found');
+  res.send(issue);
+});
+
+// Update an issue
+app.put('/issues/:id', (req, res) => {
+  const issue = issues.find(i => i.id === parseInt(req.params.id));
+  if (!issue) return res.status(404).send('Issue not found');
+
+  Object.assign(issue, req.body);
+  res.send(issue);
+});
+
+// Delete an issue
+app.delete('/issues/:id', (req, res) => {
+  const issueIndex = issues.findIndex(i => i.id === parseInt(req.params.id));
+  if (issueIndex === -1) return res.status(404).send('Issue not found');
+
+  const deletedIssue = issues.splice(issueIndex, 1);
+  res.send(deletedIssue);
+});
+
+app.listen(port, () => {
+  console.log(`Issue tracker service running on port ${port}`);
+});
